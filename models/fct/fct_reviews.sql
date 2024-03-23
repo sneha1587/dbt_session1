468cfce7479e304a32aca1a6174ded588b04bb5a
sneha.p.udayashankar@kipi.bi <sneha.p.udayashankar@kipi.bi>
2024-03-23 12:23:19 +0000

    1

--- models/fct/fct_reviews.sql
+++ models/fct/fct_reviews.sql
@@ -1,8 +1,14 @@
 {{
-    config( materialized='table')
+ config(
+ materialized = 'incremental',
+ on_schema_change='fail'
+ )
 }}
-with src_reviews as(
-    select * from {{ref('src_reviews')}}
+WITH src_reviews AS (
+ SELECT * FROM {{ ref('src_reviews') }}
 )
-select * from src_reviews
-where review_text is not null
+SELECT * FROM src_reviews
+WHERE review_text is not null
+{% if is_incremental() %}
+ AND review_date > (select max(review_date) from {{ this }})
+{% endif %}
